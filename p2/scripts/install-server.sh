a7f46631e737578f10c9c2563574582724dc185e
ajuncosa <ajm.zetazix@gmail.com>
2023-06-21 12:07:32 +0200

    add: configMaps for different html for nginx apps

--- p2/scripts/install-server.sh
+++ p2/scripts/install-server.sh
@@ -4,7 +4,12 @@ mkdir -p /etc/rancher/k3s
 cp /vagrant/confs/config.server.yml /etc/rancher/k3s/config.yaml
 curl -sfL https://get.k3s.io | sh -s - server
 
-kubectl apply -f /vagrant/confs/app-one.yaml
-kubectl apply -f /vagrant/confs/app-two.yaml
-kubectl apply -f /vagrant/confs/app-three.yaml
+kubectl apply -f /vagrant/confs/app-one/index-html-configmap.yaml
+kubectl apply -f /vagrant/confs/app-two/index-html-configmap.yaml
+kubectl apply -f /vagrant/confs/app-three/index-html-configmap.yaml
+
+kubectl apply -f /vagrant/confs/app-one/app-one.yaml
+kubectl apply -f /vagrant/confs/app-two/app-two.yaml
+kubectl apply -f /vagrant/confs/app-three/app-three.yaml
+
 kubectl apply -f /vagrant/confs/ingress.yaml
