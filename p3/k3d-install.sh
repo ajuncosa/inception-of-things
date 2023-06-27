9a10c77c9688364a29e6a9a4cf833472f6e7dcf5
Ana Juncosa <ajuncosa@student.42madrid.com>
2023-06-27 20:30:52 +0200

    add: install docker and kubectl to k3d install script

--- p3/k3d-install.sh
+++ p3/k3d-install.sh
@@ -1,7 +1,29 @@
 #!/bin/sh
 
-#TODO: install docker
-curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | sh
-k3d cluster create mycluster
-kubectl apply -f dev-namespace.yaml
-kubectl apply -f argo-namespace.yaml
+# install docker
+for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
+sudo apt-get update
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# install k3s
+sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | sh
+
+# install kubectl
+sudo apt-get install -y apt-transport-https
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-archive-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+# create cluster
+sudo k3d cluster create mycluster
+sudo kubectl apply -f dev-namespace.yaml
+sudo kubectl apply -f argo-namespace.yaml
