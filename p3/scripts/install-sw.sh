c9c293d8d6aa2457399c5bfa9473b8edf29144b2
ajuncosa <ajm.zetazix@gmail.com>
2023-07-10 16:14:02 +0200

    add: argocd application CD

--- p3/scripts/install-sw.sh
+++ p3/scripts/install-sw.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # Install docker
 for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do sudo apt-get remove $pkg; done
@@ -22,28 +22,3 @@ curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --de
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubectl
-
-# Create k3d cluster
-sudo k3d cluster create mycluster -p "80:80@loadbalancer" -p "443:443@loadbalancer" -p "8888:30036@loadbalancer"
-
-# Create namespaces
-kubectl apply -f iot-apavel/app/dev-namespace.yaml
-kubectl apply -f argo-namespace.yaml
-
-# Create app resources
-kubectl apply -f iot-apavel/app/deployment.yaml
-kubectl apply -f iot-apavel/app/service.yaml
-
-# Create argocd resources
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
-
-# Turn argocd-server service into a LoadBalancer
-kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
-
-# Instalar CLI argocd
-# brew install argocd #MACOS
-# TODO: Instalar CLI argocd en maquina virtual de 42
-
-# Change argocd admin user's password
-# TODO: meter la contraseña con un secret o un kustomization o algo
-kubectl -n argocd patch secret argocd-secret -p '{"stringData":  {"admin.password": "$2y$12$Kg4H0rLL/RVrWUVhj6ykeO3Ei/YqbGaqp.jAtzzUSJdYWT6LUh/n6", "admin.passwordMtime": "'$(date +%FT%T%Z)'"}}' #mysupersecretpassword
--- p3/scripts/setup-cluster.sh
+++ p3/scripts/setup-cluster.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Create namespaces
-kubectl apply -f iot-apavel/app/dev-namespace.yaml
-kubectl apply -f argo-namespace.yaml
+# Create k3d cluster
+k3d cluster create mycluster -p "80:80@loadbalancer" -p "443:443@loadbalancer" -p "8888:30036@loadbalancer"
 
-# Create app resources
-kubectl apply -f iot-apavel/app/deployment.yaml
-kubectl apply -f iot-apavel/app/service.yaml
+# Create namespaces
+kubectl apply -f ../confs/dev-namespace.yaml
+kubectl apply -f ../confs/argo-namespace.yaml
 
 # Create argocd resources
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
@@ -14,10 +13,13 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 # Turn argocd-server service into a LoadBalancer
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-# Instalar CLI argocd
-# brew install argocd #MACOS
-# TODO: Instalar CLI argocd en maquina virtual de 42
+# Wait for argocd-server service
+echo -e '\033[1;34mWaiting for argocd-server service to become available...\033[0m'
+kubectl wait deployment/argocd-server -n argocd --for=condition=Available=true --timeout=300s
 
-# Change admin user's password
+# Change argocd admin user's password
 # TODO: meter la contraseña con un secret o un kustomization o algo
 kubectl -n argocd patch secret argocd-secret -p '{"stringData":  {"admin.password": "$2y$12$Kg4H0rLL/RVrWUVhj6ykeO3Ei/YqbGaqp.jAtzzUSJdYWT6LUh/n6", "admin.passwordMtime": "'$(date +%FT%T%Z)'"}}' #mysupersecretpassword
+
+# Create app
+kubectl apply -f ../confs/application.yaml
