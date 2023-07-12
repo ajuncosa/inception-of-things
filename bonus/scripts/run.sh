cdffc7b31a9bd90b123e9facc004cacaafece7e2
ajuncosa <ajm.zetazix@gmail.com>
2023-07-12 14:55:27 +0200

    add useless gitlab try

---
+++ bonus/scripts/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo -e '\033[1;34mInstalling software prerequisites...\033[0m'
+#bash install-sw.sh
+
+echo -e '\033[1;34mSetting up k3d cluster and deploying app...\033[0m'
+sudo bash setup-cluster.sh
--- bonus/scripts/setup-cluster.sh
+++ bonus/scripts/setup-cluster.sh
@@ -4,8 +4,23 @@
 k3d cluster create mycluster -p "80:80@loadbalancer" -p "443:443@loadbalancer" -p "8888:8888@loadbalancer"
 
 # Create namespaces
-kubectl apply -f ../confs/dev-namespace.yaml
-kubectl apply -f ../confs/argo-namespace.yaml
+kubectl create namespace dev
+kubectl create namespace argocd
+kubectl create namespace gitlab
+
+
+# Install GitLab
+helm repo add gitlab https://charts.gitlab.io/
+helm repo update
+helm upgrade --install gitlab gitlab/gitlab \
+	--timeout 600s \
+	--values ../confs/gitlab-values.yaml \
+	--namespace gitlab
+#  --set global.hosts.domain=localhost \
+#  --set global.hosts.externalIP=127.0.0.1 \
+#  --set certmanager-issuer.email=me@example.com \
+#  --set postgresql.image.tag=13.6.0
+
 
 # Create argocd resources
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
@@ -13,8 +28,8 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 # Turn argocd-server service into a LoadBalancer
 kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
 
-# Wait for argocd-server depl
-echo -e '\033[1;34mWaiting for argocd-server to become available...\033[0m'
+# Wait for argocd-server service
+echo -e '\033[1;34mWaiting for argocd-server service to become available...\033[0m'
 kubectl wait deployment/argocd-server -n argocd --for=condition=Available=true --timeout=300s
 
 # Change argocd admin user's password
