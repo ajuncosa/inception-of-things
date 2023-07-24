b96e7d43d9578dbd715f16a8fb1af092150730b4
ajuncosa <ajm.zetazix@gmail.com>
2023-07-24 19:07:35 +0200

    p1: switch ubuntu for debian

--- p1/scripts/install-agent.sh
+++ p1/scripts/install-agent.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+apt-get update
+apt-get install curl -y
+
 mkdir -p /etc/rancher/k3s
 cp /vagrant/confs/config.agent.yml /etc/rancher/k3s/config.yaml
 curl -sfL https://get.k3s.io | sh -s - agent
--- p1/scripts/install-server.sh
+++ p1/scripts/install-server.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+apt-get update
+apt-get install curl -y
+
 mkdir -p /etc/rancher/k3s
 cp /vagrant/confs/config.server.yml /etc/rancher/k3s/config.yaml
 curl -sfL https://get.k3s.io | sh -s - server
