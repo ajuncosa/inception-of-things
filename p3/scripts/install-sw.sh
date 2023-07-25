03c4cfce4f6faf526f25a81f382011b29353170b
Adrian Pavel <adrian199909@gmail.com>
2023-07-25 18:47:23 +0200

    last minute fixes

--- p3/scripts/install-sw.sh
+++ p3/scripts/install-sw.sh
@@ -5,16 +5,17 @@ for pkg in docker.io docker-doc docker-compose podman-docker containerd runc; do
 sudo apt-get update
 sudo apt-get install -y ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Install k3d
-sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | sh
+sudo curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
 # Install kubectl
 sudo apt-get install -y apt-transport-https
